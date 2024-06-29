704a30f7e21c85b752338084eb3b2a8c1e61ff3e
James Feng Cao <jamesfengcao@torapp.info>
2024-06-29 08:01:33 +0800

    add hash2editable.js

--- js/hash2textarea.js
+++ js/hash2textarea.js
@@ -1,1 +1,1 @@
-(async ()=>{let d=document;let t=d.location.hash.substring(1);if(!t)return;t=decodeURI(t);while(true){let ta=d.querySelector('textarea');if(ta){ta.value=t;ta.dispatchEvent(new InputEvent('input'));ta.focus();return;}await new Promise(resolve=>setTimeout(resolve,400));}})()
+(async ()=>{let d=document;let t=d.location.hash.slice(1);if(!t)return;t=decodeURI(t);while(true){let ta=d.querySelector('textarea');if(ta){ta.value=t;ta.dispatchEvent(new InputEvent('input'));ta.focus();return;}await new Promise(resolve=>setTimeout(resolve,400));}})()
