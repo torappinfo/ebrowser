4d8dc30e1b105a5485c50420f52326eff837a463
James Feng Cao <jamesfengcao@torapp.info>
2024-07-01 09:34:07 +0800

    revert back w/o ipc

--- webview.js
+++ webview.js
@@ -7,7 +7,7 @@ You should have received a copy of the GNU General Public License along with thi
 */
 const {
   app, BrowserWindow, Menu, shell, clipboard,
-  session, protocol, net, dialog, ipcMain
+  session, protocol, net, dialog
 } = require('electron')
 let win;
 
@@ -182,10 +182,6 @@ app.on ('web-contents-created', (event, contents) => {
     contents.session.webRequest.onBeforeRequest(interceptRequest);
     //contents.on('did-finish-load',()=>{cbFinishLoad(contents)});
   }
-});
-
-ipcMain.on('command', (event, cmd) => {
-  addrCommand(cmd);
 });
 
 function addrCommand(cmd){
