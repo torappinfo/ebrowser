35d4303830a65decae699a974af89a435a0cf2b6
James Feng Cao <jamesfengcao@torapp.info>
2024-11-18 16:05:10 +0800

    add uweb2ta.js

---
+++ js/uweb2ta.js
@@ -0,0 +1,1 @@
+(async ()=>{let d=document;let t=location.href;t=t.substring(t.indexOf('uweb=')+5);t=decodeURI(t);while(true){let ta=d.querySelector('textarea');if(ta){ta.value=t;ta.dispatchEvent(new InputEvent('input'));ta.focus();return;}await new Promise(resolve=>setTimeout(resolve,400));}})()
