de87fe9fffdbb91ea17d63f6866e00741040b599
James Feng Cao <jamesfengcao@torapp.info>
2024-12-08 11:43:50 +0800

    optimize addrCommand parsing

--- js/s2ta.js
+++ js/s2ta.js
@@ -1,1 +1,1 @@
-(async ()=>{let d=document;let t=location.search;t=t.substring(t.indexOf('??')+2);t=decodeURI(t);while(true){let ta=d.querySelector('textarea');if(ta){ta.value=t;ta.dispatchEvent(new InputEvent('input'));ta.focus();return;}await new Promise(resolve=>setTimeout(resolve,400));}})()
+(async ()=>{let d=document;let t=location.search;t=t.substring(t.indexOf('??')+2);t=decodeURI(t);while(true){await new Promise(resolve=>setTimeout(resolve,100));let ta=d.querySelector('textarea');if(ta){ta.value=t;ta.dispatchEvent(new InputEvent('input'));ta.focus();return;}}})()
