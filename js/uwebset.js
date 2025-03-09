9164b6b6e0eb5e57f5bf2eb2b580c4b6d6debfe4
James Feng Cao <jamesfengcao@torapp.info>
2025-03-09 15:07:16 +0800

    add uwebset.js

---
+++ js/uwebset.js
@@ -0,0 +1,19 @@
+(async ()=>{let t=location.href;let i=t.indexOf('uweb=');
+if(i<3) return;
+let sleep=(ms)=>{return new Promise(resolve=>setTimeout(resolve,ms))};
+function setVal(e,v){
+  const inputEvent = new Event('input', { bubbles: true, composed: true });
+  const nativeInputVSetter = Object.getOwnPropertyDescriptor(
+    window.HTMLTextAreaElement.prototype,'value').set;
+  nativeInputVSetter.call(e,v);
+  e.dispatchEvent(inputEvent);}
+let bAppend = false;
+{let w=t.charCodeAt(i-1)-48;if(w>=0&&w<=9){w=w*100+(t.charCodeAt(i-2)-48)*1000;await sleep(w);
+if(65===t.charCodeAt(i-3))bAppend=true;}}
+if(i+5===t.length)return;
+t=decodeURI(t.substring(i+5));
+while(true){let ta=document.querySelector('textarea');
+if(ta){
+if(bAppend)t=ta.value+t;
+setVal(ta,t);
+return;}await sleep(400);}})()
