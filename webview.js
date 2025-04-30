8cd8a6f1a7ccee692bca949597ad2b9675f18c29
James Feng Cao <jamesfengcao@torapp.info>
2025-04-30 19:26:48 +0800

    increase initial windows size for windows OSes

--- webview.js
+++ webview.js
@@ -95,7 +95,7 @@ async function createWindow () {
   })();
 
   win = new BrowserWindow(
-    {width: 800, height: 600,autoHideMenuBar: true,
+    {width: 1200, height: 800,autoHideMenuBar: true,
      webPreferences: {
        nodeIntegration: true,
        contextIsolation: false,
